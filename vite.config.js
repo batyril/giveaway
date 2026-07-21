35cc15da561ba500ad2e28f7400fdfc44a1fc31d
ilnaz <237x237@gmail.com>
2026-07-21 11:34:14 +0300

    feat: 🎸 add build

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,10 @@ import tailwindcss from '@tailwindcss/vite'
 import path from "path"
 
 
+
+
 export default defineConfig({
+  base: '/giveaway/',
   plugins: [react(),tailwindcss()],resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
